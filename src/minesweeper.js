96ee8831eef8bb8d8394aac518d2657e2ad9c25a
Susan Harkey <susanharkey@gmail.com>
2017-12-31 17:21:19 -0600

    Fix missing parameter in generate bomb board method

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,9 +1,29 @@
+class Game {
+  constructor(numberOfRows, numberOfColumns, numberOfBombs) {
+    this._board = new Board(numberOfRows, numberOfColumns, numberOfBombs);
+  }
+
+  playMove(rowIndex, columnIndex) {
+    this._board.flipTile(rowIndex, columnIndex);
+    if (this._board.playerBoard[rowIndex][columnIndex] === 'B'){
+      console.log("Game over!");
+      this._board.print();
+    } else if (!this._board.hasSafeTiles) {
+      console.log("You won!");
+      this._board.print();
+    } else {
+      console.log("Current board: ");
+      this._board.print();
+    }
+  }
+}
+
 class Board {
   constructor(numberOfRows, numberOfColumns, numberOfBombs) {
     this._numberOfTiles = numberOfRows * numberOfColumns;
     this._numberOfBombs = numberOfBombs;
-    this._playerBoard = generatePlayerBoard(numberOfRows, numberOfColumns);
-    this._bombBoard = generateBombBoard(numberOfRows, numberOfColumns);
+    this._playerBoard = Board.generatePlayerBoard(numberOfRows, numberOfColumns);
+    this._bombBoard = Board.generateBombBoard(numberOfRows, numberOfColumns, numberOfBombs);
   }
 
   get playerBoard() {
@@ -47,7 +67,7 @@ class Board {
   }
 
   print() {
-    console.log(board.map(row => row.join(' | ')).join('\n'));
+    console.log(this._playerBoard.map(row => row.join(' | ')).join('\n'));
   };
 
   static generatePlayerBoard(numberOfRows, numberOfColumns) {
@@ -65,51 +85,37 @@ class Board {
     return board;
   };
 
+  static generateBombBoard(numberOfRows, numberOfColumns, numberOfBombs) {
+    let board = [];
+    for (let i = 0; i < numberOfRows; i++){
+      // Create new array for each row
+      let row = [];
+      for (let j = 0; j < numberOfColumns; j++){
+        // Create new column within each row array - set to empty
+        row.push(null);
+      };
+      // Add rows to board
+      board.push(row);
+    };
+    let numberOfBombsPlaced = 0;
+    while (numberOfBombsPlaced < numberOfBombs) {
+      // This while loop can place bombs on top of other bombs.
+      // I will fix this later with the control flow lesson.
+      // Get random row for bomb
+      let randomRowIndex = Math.floor(Math.random() *  numberOfRows);
+      // Get random column for bomb
+      let randomColumnIndex = Math.floor(Math.random() *  numberOfColumns);
+      // Set random row/column index in board array to bomb
+        if (board[randomRowIndex][randomColumnIndex] !== 'B') {
+          board[randomRowIndex][randomColumnIndex] = 'B';
+          // Increment numberOfBombsPlaced
+          numberOfBombsPlaced++;
+        }
+    };
+    return board;
+  };
+
 }
 
-const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
-  let board = [];
-  for (let i = 0; i < numberOfRows; i++){
-    // Create new array for each row
-    let row = [];
-    for (let j = 0; j < numberOfColumns; j++){
-      // Create new column within each row array - set to empty
-      row.push(null);
-    };
-    // Add rows to board
-    board.push(row);
-  };
-  let numberOfBombsPlaced = 0;
-  while (numberOfBombsPlaced < numberOfBombs) {
-    // This while loop can place bombs on top of other bombs.
-    // I will fix this later with the control flow lesson.
-    // Get random row for bomb
-    let randomRowIndex = Math.floor(Math.random() *  numberOfRows);
-    // Get random column for bomb
-    let randomColumnIndex = Math.floor(Math.random() *  numberOfColumns);
-    // Set random row/column index in board array to bomb
-      if (board[randomRowIndex][randomColumnIndex] !== 'B') {
-        board[randomRowIndex][randomColumnIndex] = 'B';
-        // Increment numberOfBombsPlaced
-        numberOfBombsPlaced++;
-      }
-  };
-  return board;
-};
-
-const playerBoard = generatePlayerBoard(3, 4);
-const bombBoard = generateBombBoard(3, 4, 5);
-
-console.log('Player Board: ');
-
-printBoard(playerBoard);
-
-console.log('Bomb Board: ');
-
-printBoard(bombBoard);
-
-flipTile(playerBoard, bombBoard, 1, 0);
-
-console.log('Updated Player Board: ');
-
-printBoard(playerBoard);
+const g = new Game(3, 3, 3);
+g.playMove(0,0);
